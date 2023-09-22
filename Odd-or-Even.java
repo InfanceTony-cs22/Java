88bea74d4b03703ecdc700e82c232b76776b098b
Infance Tony.E <138364215+InfanceTony-cs22@users.noreply.github.com>
2023-09-22 22:29:30 +0530

    Odd-or-Even.java

---
+++ Odd-or-Even.java
@@ -0,0 +1,11 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+        int number = 7;
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+    }
+}
