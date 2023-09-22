b4f6bfb7cacb32624ee7c3f8b7d2aa3250435026
Infance Tony.E <138364215+InfanceTony-cs22@users.noreply.github.com>
2023-09-22 21:54:54 +0530

    Rectangle.java

---
+++ rectangle.java
@@ -0,0 +1,13 @@
+public class RectangleArea {
+    public static void main(String[] args) {
+        // Declare variables for the length and width of the rectangle
+        double length = 5.5;
+        double width = 3.0;
+
+        // Calculate the area of the rectangle
+        double area = length * width;
+
+        // Display the result
+        System.out.println("The area of the rectangle is: " + area);
+    }
+}
