6e3c9a654276f32888ed2f67fafa1961bc725c8c
Infance Tony.E <138364215+InfanceTony-cs22@users.noreply.github.com>
2023-10-02 19:37:28 +0530

    Installing-java.java

---
+++ Installing-java.java
@@ -0,0 +1,50 @@
+Setting Up Your Java Environment
+
+  Topic:
+
+On Day 1, we focus on setting up your Java development environment. This includes installing Java and a code editor or integrated development environment (IDE) like Visual Studio Code (VS Code).
+
+
+  Step 1: Install Java
+
+To start programming in Java, you need to have Java Development Kit (JDK) installed on your computer. Follow these steps to install Java:
+
+Visit the Oracle JDK download page (for Oracle JDK) or the AdoptOpenJDK website (for OpenJDK).
+Download the appropriate JDK version for your operating system (e.g., Windows, macOS, Linux).
+Run the installer and follow the installation instructions.
+
+  
+Step 2: Verify Java Installation
+
+After installing Java, you should verify that it's correctly installed. Open a terminal (Command Prompt on Windows) and type the following command:
+
+    java -version
+
+      
+      Step 3: Install a Code Editor or IDE
+
+You'll also need a code editor or integrated development environment (IDE) to write and run Java code efficiently. Visual Studio Code (VS Code) is a popular choice for Java development. Here's how to set up VS Code for Java:
+
+Install VS Code if you haven't already: Download VS Code from the official website (https://code.visualstudio.com/) and install it.
+
+Install the "Java Extension Pack" for VS Code: Open VS Code, go to the Extensions view (click on the square icon on the left sidebar or press Ctrl+Shift+X), search for "Java Extension Pack," and install it.
+
+  
+
+Step 4: Create Your First Java Program
+
+Now that your Java environment is set up, you can create your first Java program. In VS Code:
+
+Create a new Java file by clicking on the "File" menu, selecting "New File," and saving it with a .java extension (e.g., HelloWorld.java).
+
+
+Step 5: Compile and Run the Program
+
+Open a terminal within VS Code (Terminal > New Terminal).
+
+Navigate to the directory where your Java file is located using the cd command (e.g., cd /path/to/your/java/program).
+
+Compile your Java program using the javac command followed by the filename
+
+
+  
