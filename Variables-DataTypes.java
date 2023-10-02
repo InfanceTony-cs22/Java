037408e40ca25b87b6732ea87e2509e54b7db4cc
Infance Tony.E <138364215+InfanceTony-cs22@users.noreply.github.com>
2023-10-02 19:42:12 +0530

    Variables-DataTypes.java
    Concepts with example

---
+++ Variables-DataTypes.java
@@ -0,0 +1,50 @@
+DATA TYPE AND VARIABLES:
+-----------------------
+
+
+Explanation:
+------------
+
+In Java, variables are used to store data, and every variable has a data type that specifies what kind of data it can hold. Here are some common data types in Java:
+
+int: Used for storing integers (whole numbers).
+double: Used for storing floating-point numbers (numbers with decimal points).
+boolean: Used for storing true or false values.
+char: Used for storing a single character.
+String: Used for storing sequences of characters (text).
+
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+  int age = 30;
+double price = 19.99;
+boolean isJavaFun = true;
+char grade = 'A';
+String message = "Hello, Java!";
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  program:
+-----------
+  public class RectangleArea {
+    public static void main(String[] args) {
+        // Declare variables for the length and width of the rectangle
+        double length = 5.5;
+        double width = 3.0;
+
+        // Calculate the area of the rectangle
+        double area = length * width;
+
+        // Display the result
+        System.out.println("The area of the rectangle is: " + area);
+    }
+}
+
+  In this program:
+
+We declare two variables, length and width, both of type double, to represent the dimensions of a rectangle.
+We calculate the area of the rectangle by multiplying the length and width and store the result in the area variable.
+Finally, we use System.out.println to display the calculated area on the console.
+When you run this program, it will calculate and display the area of the rectangle based on the provided length and width.
+
+Feel free to run this program in your Java development environment and experiment with different values for length and width to see how the area changes. This will help reinforce your understanding of variables and data types in Java.
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    
