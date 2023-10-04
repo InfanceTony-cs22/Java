a98d303a265aaaeb7f7d911af5dcb747dd42b232
Infance Tony.E <138364215+InfanceTony-cs22@users.noreply.github.com>
2023-10-04 21:11:33 +0530

    odd-even.java
    using-if-else

---
+++ odd-even.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class OddEven {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input a number
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        // Check if the number is odd or even
+        if (number % 2 == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+
+        scanner.close();
+    }
+}
