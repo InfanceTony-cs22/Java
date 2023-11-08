4231db5a95f6b528dbd5daa45fddc3a7ca11339b
Infance Tony.E <138364215+InfanceTony-cs22@users.noreply.github.com>
2023-11-08 19:57:08 +0530

    Create Substring.java

---
+++ Substring.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        String substring=S.substring(start,end );
+        System.out.println(substring);
+        in.close();
+    }
+}
