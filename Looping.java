c5f1a121886bfa2aafadc1d584e6b5bfd6d783b4
Infance Tony.E <138364215+InfanceTony-cs22@users.noreply.github.com>
2023-11-10 20:04:35 +0530

    Create Looping.java

---
+++ Looping.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        // Loop to print the first 10 multiples of N
+        for (int i = 1; i <= 10; i++) {
+            int result = N * i;
+            System.out.println(N + " x " + i + " = " + result);
+        }
+
+        bufferedReader.close();
+    }
+}
