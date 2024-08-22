070b1c41c14bb0a8a3540a217fb4d67321654dfd
Infance Tony.E <138364215+InfanceTony-cs22@users.noreply.github.com>
2024-08-22 21:08:39 +0530

    Update 1-Dimensional-Array.java

--- 1-Dimensional-Array.java
+++ 1-Dimensional-Array.java
@@ -12,6 +12,7 @@ public class Solution {
             a[i]=scan.nextInt();
         }
         scan.close();
+..
 
         // Prints each sequential element in array a
         for (int i = 0; i < a.length; i++) {
